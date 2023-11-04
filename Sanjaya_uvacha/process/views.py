3db0d58e3fed9ba111f2bc37c1aec1a0e47e18e0
RithinChand2004 <97657716+RithinChand2004@users.noreply.github.com>
2023-11-04 17:37:43 +0530

    Added route function and surroundings functions

--- Sanjaya_uvacha/process/views.py
+++ Sanjaya_uvacha/process/views.py
@@ -198,3 +198,262 @@ def findObjects(request):
         # print("   Error reason: {}".format(error_details.reason))
         # print("   Error code: {}".format(error_details.error_code))
         # print("   Error message: {}".format(error_details.message))
+        
+def busroute(request):
+    text = (request.GET.get('text'))
+    print(text)
+    text = text.lower()
+    text = text.split(' ')
+    city = text[-1]
+    print(city)
+    service_options = sdk.VisionServiceOptions("https://dristi.cognitiveservices.azure.com/",
+                                               "ec8f8825967f482699df9b8080d3d826")
+
+    vision_source = sdk.VisionSource(
+        url="https://bumble.blob.core.windows.net/data/frame123.jpg")
+
+    analysis_options = sdk.ImageAnalysisOptions()
+
+    analysis_options.features = (
+        # sdk.ImageAnalysisFeature.CAPTION |
+        # sdk.ImageAnalysisFeature.DENSE_CAPTIONS |
+        # sdk.ImageAnalysisFeature.OBJECTS |
+        # sdk.ImageAnalysisFeature.PEOPLE |
+        sdk.ImageAnalysisFeature.TEXT |
+        sdk.ImageAnalysisFeature.TAGS
+    )
+
+    analysis_options.language = "en"
+
+    analysis_options.gender_neutral_caption = True
+
+    image_analyzer = sdk.ImageAnalyzer(
+        service_options, vision_source, analysis_options)
+
+    result = image_analyzer.analyze()
+    tagt = ''
+
+    if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+
+        print(" Image height: {}".format(result.image_height))
+        print(" Image width: {}".format(result.image_width))
+        print(" Model version: {}".format(result.model_version))
+
+        
+        if result.tags is not None:
+            print(" Tags:")
+            for tag in result.tags:
+                print("   '{}', Confidence {:.4f}".format(
+                    tag.name, tag.confidence))
+                tagt += tag.name + ' '
+        
+        if 'bus' in tagt:
+            t = ''
+            if result.text is not None:
+                print(" Text:")
+                for line in result.text.lines:
+                    points_string = "{" + ", ".join([str(int(point))
+                                                    for point in line.bounding_polygon]) + "}"
+                    print("   Line: '{}', Bounding polygon {}".format(
+                        line.content, points_string))
+                    for word in line.words:
+                        points_string = "{" + ", ".join([str(int(point))
+                                                        for point in word.bounding_polygon]) + "}"
+                        print("     Word: '{}', Bounding polygon {}, Confidence {:.4f}"
+                            .format(word.content, points_string, word.confidence))
+                        t += word.content + ' '
+            t = t.lower()
+            if city[:-1] in t:
+                speakOut('This bus goes to ' + city)
+                return JsonResponse({'bus': 'This bus goes to ' + city})
+            else:
+                speakOut('This bus does not go to ' + city)
+                return JsonResponse({'bus': 'This bus does not go to ' + city})
+        else:
+            speakOut('No bus found')
+            return JsonResponse({'bus': 'No bus found'})
+
+        # result_details = sdk.ImageAnalysisResultDetails.from_result(result)
+        # print(" Result details:")
+        # print("   Image ID: {}".format(result_details.image_id))
+        # print("   Result ID: {}".format(result_details.result_id))
+        # print("   Connection URL: {}".format(result_details.connection_url))
+        # print("   JSON result: {}".format(result_details.json_result))
+
+    else:
+
+        error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
+        print(" Analysis failed.")
+        # print("   Error reason: {}".format(error_details.reason))
+        # print("   Error code: {}".format(error_details.error_code))
+        # print("   Error message: {}".format(error_details.message))
+
+def danger(request):
+    service_options = sdk.VisionServiceOptions("https://dristi.cognitiveservices.azure.com/",
+                                               "ec8f8825967f482699df9b8080d3d826")
+
+    vision_source = sdk.VisionSource(
+        url="https://bumble.blob.core.windows.net/data/frame123.jpg")
+
+    analysis_options = sdk.ImageAnalysisOptions()
+
+    analysis_options.features = (
+        sdk.ImageAnalysisFeature.CAPTION |
+        # sdk.ImageAnalysisFeature.DENSE_CAPTIONS |
+        # sdk.ImageAnalysisFeature.OBJECTS |
+        # sdk.ImageAnalysisFeature.PEOPLE |
+        # sdk.ImageAnalysisFeature.TEXT |
+        sdk.ImageAnalysisFeature.TAGS
+    )
+
+    analysis_options.language = "en"
+
+    analysis_options.gender_neutral_caption = True
+
+    image_analyzer = sdk.ImageAnalyzer(
+        service_options, vision_source, analysis_options)
+
+    result = image_analyzer.analyze()
+    t = ''
+    if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+
+        print(" Image height: {}".format(result.image_height))
+        print(" Image width: {}".format(result.image_width))
+        print(" Model version: {}".format(result.model_version))
+
+        if result.caption is not None:
+            print(" Caption:")
+            print("   '{}', Confidence {:.4f}".format(
+                result.caption.content, result.caption.confidence))
+        t = ''
+        if result.tags is not None:
+            print(" Tags:")
+            for tag in result.tags:
+                print("   '{}', Confidence {:.4f}".format(
+                    tag.name, tag.confidence))
+                print(tag.name,'weapon',tag.name == 'weapon')
+                t += tag.name + ' '
+                if 'weapon' == tag.name:
+                    speakOut('danger ' + result.caption.content)
+                    return JsonResponse({'danger': ('danger ' + result.caption.content)})
+            
+        
+        
+
+        # result_details = sdk.ImageAnalysisResultDetails.from_result(result)
+        # print(" Result details:")
+        # print("   Image ID: {}".format(result_details.image_id))
+        # print("   Result ID: {}".format(result_details.result_id))
+        # print("   Connection URL: {}".format(result_details.connection_url))
+        # print("   JSON result: {}".format(result_details.json_result))
+
+    else:
+
+        error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
+        print(" Analysis failed.")
+        # print("   Error reason: {}".format(error_details.reason))
+        # print("   Error code: {}".format(error_details.error_code))
+        # print("   Error message: {}".format(error_details.message))
+    return JsonResponse({'danger': f"Objects are \n {t}. \n No weapons are detected"})
+
+def surroundings(request):
+    service_options = sdk.VisionServiceOptions("https://dristi.cognitiveservices.azure.com/",
+                                               "ec8f8825967f482699df9b8080d3d826")
+
+    vision_source = sdk.VisionSource(
+        url="https://bumble.blob.core.windows.net/data/frame123.jpg")
+
+    analysis_options = sdk.ImageAnalysisOptions()
+
+    analysis_options.features = (
+        sdk.ImageAnalysisFeature.CAPTION |
+        sdk.ImageAnalysisFeature.DENSE_CAPTIONS 
+        # sdk.ImageAnalysisFeature.OBJECTS |
+        # sdk.ImageAnalysisFeature.PEOPLE |
+        # sdk.ImageAnalysisFeature.TEXT |
+        # sdk.ImageAnalysisFeature.TAGS
+    )
+
+    analysis_options.language = "en"
+
+    analysis_options.gender_neutral_caption = True
+
+    image_analyzer = sdk.ImageAnalyzer(
+        service_options, vision_source, analysis_options)
+
+    result = image_analyzer.analyze()
+    cap = ''
+    if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+
+        print(" Image height: {}".format(result.image_height))
+        print(" Image width: {}".format(result.image_width))
+        print(" Model version: {}".format(result.model_version))
+
+        if result.caption is not None:
+            print(" Caption:")
+            print("   '{}', Confidence {:.4f}".format(
+                result.caption.content, result.caption.confidence))
+            cap = result.caption.content
+
+        if result.dense_captions is not None:
+            print(" Dense Captions:")
+            for caption in result.dense_captions:
+                print("   '{}', {}, Confidence: {:.4f}".format(
+                    caption.content, caption.bounding_box, caption.confidence))
+        
+        speakOut(cap)
+        return JsonResponse({'caption': cap})
+
+        
+        # result_details = sdk.ImageAnalysisResultDetails.from_result(result)
+        # print(" Result details:")
+        # print("   Image ID: {}".format(result_details.image_id))
+        # print("   Result ID: {}".format(result_details.result_id))
+        # print("   Connection URL: {}".format(result_details.connection_url))
+        # print("   JSON result: {}".format(result_details.json_result))
+
+    else:
+
+        error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
+        print(" Analysis failed.")
+        # print("   Error reason: {}".format(error_details.reason))
+        # print("   Error code: {}".format(error_details.error_code))
+        # print("   Error message: {}".format(error_details.message))
+
+
+def isInsideBox(position, x, y, w, h):
+        if position[0] >= x and position[0] <= x + w:
+            if position[1] >= y and position[1] <= y + h:
+                return True
+        return False
+
+
+def retriveCaption():
+    service_options = sdk.VisionServiceOptions("https://dristi.cognitiveservices.azure.com/",
+                                               "ec8f8825967f482699df9b8080d3d826")
+
+    vision_source = sdk.VisionSource(
+        url="https://bumble.blob.core.windows.net/data/frame123.jpg")
+
+    analysis_options = sdk.ImageAnalysisOptions()
+
+    analysis_options.features = (
+        sdk.ImageAnalysisFeature.CAPTION |
+        sdk.ImageAnalysisFeature.DENSE_CAPTIONS
+        # sdk.ImageAnalysisFeature.OBJECTS |
+        # sdk.ImageAnalysisFeature.PEOPLE |
+        # sdk.ImageAnalysisFeature.TEXT |
+        # sdk.ImageAnalysisFeature.TAGS
+    )
+
+    analysis_options.language = "en"
+
+    analysis_options.gender_neutral_caption = True
+
+    image_analyzer = sdk.ImageAnalyzer(
+        service_options, vision_source, analysis_options)
+
+    result = image_analyzer.analyze()
+
+    if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+        return result
